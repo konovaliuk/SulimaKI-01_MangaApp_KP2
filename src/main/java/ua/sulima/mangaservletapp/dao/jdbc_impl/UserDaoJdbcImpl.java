efef632ffe8623c8ec5817b7b063ea277ea61fc1
SuviGod <sulima.ivan@lll.kpi.ua>
2023-03-15 00:40:30 +0200

    fix: modified UserDaoJdbcImpl

--- src/main/java/ua/sulima/mangaservletapp/dao/jdbc_impl/UserDaoJdbcImpl.java
+++ src/main/java/ua/sulima/mangaservletapp/dao/jdbc_impl/UserDaoJdbcImpl.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.ArrayUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ua.sulima.mangaservletapp.dao.UserDao;
+import ua.sulima.mangaservletapp.dao.mapper.RoleMapper;
 import ua.sulima.mangaservletapp.dao.mapper.UserMapper;
 import ua.sulima.mangaservletapp.entity.Role;
 import ua.sulima.mangaservletapp.entity.User;
@@ -12,6 +13,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -25,17 +28,46 @@ public class UserDaoJdbcImpl extends ConnectionDaoJdbcImpl implements UserDao {
     public Optional<User> findById(Long id) {
 
         User user = null;
-        String statement = "SELECT * FROM mangaapp.users WHERE id = ?;";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
-            preparedStatement.setLong(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            System.out.println();
+        String findUserStatement = "SELECT * FROM mangaapp.users WHERE id = ?;";
+        String findUserRolesStatement = "SELECT r.*\n" +
+                "FROM mangaapp.role r\n" +
+                "JOIN mangaapp.user_role ur\n" +
+                "ON r.id = ur.role_id\n" +
+                "WHERE ur.user_id = ?;";
+        try (PreparedStatement findUserPreparedStatement =
+                     connection.prepareStatement(findUserStatement);
+             PreparedStatement findUserRolesPreparedStatement =
+                     connection.prepareStatement(findUserRolesStatement)) {
+
+            connection.setAutoCommit(false);
+
+            findUserPreparedStatement.setLong(1, id);
+            ResultSet resultSet = findUserPreparedStatement.executeQuery();
             if(resultSet.next()){
                 user = new UserMapper().retrieveFromResultSet(resultSet);
             }
-//            user = new UserMapper().retrieveFromResultSet(resultSet);
+
+            findUserRolesPreparedStatement.setLong(1, id);
+            resultSet = findUserRolesPreparedStatement.executeQuery();
+            List<Role> userRolesList = new ArrayList<>();
+            RoleMapper roleMapper = new RoleMapper();
+            while(resultSet.next()){
+                userRolesList.add(roleMapper.retrieveFromResultSet(resultSet));
+            }
+            user.setRoles(userRolesList);
+            connection.commit();
+            connection.setAutoCommit(true);
         } catch (SQLException e) {
             e.printStackTrace();
+            if (connection != null) {
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    connection.rollback();
+                    connection.setAutoCommit(true);
+                } catch (SQLException exception) {
+                    e.printStackTrace();
+                }
+            }
         }
         return Optional.ofNullable(user);
     }
